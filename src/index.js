1577570e7f86adebfb39b9399e783ad4be61cb93
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-05-03 10:48:57 +0100

    Feat: Remove fullscreen and other controls from the map Options

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ function initMap() {
   var mapOptions = {
     center: {lat: 51.4275898, lng: -0.308368},
     zoom: 13,
-    mapTypeControl: false
+    mapTypeControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false
   };
 
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
