fa1903f5ee353ffbfbb95bda970ff2f36ae0d3dd
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-05-03 10:46:25 +0100

    Feat: Create basic html with map and header and styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './css/styles.css';
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+'use strict';
+
+
+// Set all global variables
+var map;
+
+
+// Callback to Google MapsAPI
+function initMap() {
+  var mapOptions = {
+    center: {lat: 51.4275898, lng: -0.308368},
+    zoom: 13,
+    mapTypeControl: false
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
+
+/*
+ *
+ * ViewModel
+ *
+ */
+ var ViewModel = function() {
+  var self = this;
+
+  var infowindow = new google.maps.InfoWindow({
+    maxwidth: 250,
+  });
+
+ }
+
+/* Google maps callback sets off the execution of this app */
+window.initApp = function() {
+  initMap();
+  // Initialize the ModelView and bind it to the View.
+  ko.applyBindings(new ViewModel());
+}
+
+
+
+/*
 
 async function component() {
   var element = document.createElement('div');
@@ -25,7 +61,7 @@ async function component() {
 component().then(component => {
   document.body.appendChild(component);
 });
-/*
+
 let element = component(); // store the element to re-render on print.js change
 document.body.appendChild(element);
 
