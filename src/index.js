3e5ed011ebc1d7e41a514fbeba2300a851ba2a59
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-05-03 10:57:36 +0100

    Feat: refactor comments and add Forces Model

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 'use strict';
 
 
-// Set all global variables
+/* ==== Global Variables ==== */
+
 var map;
 
 
-// Callback to Google MapsAPI
+/* ==== Callback to Google Map API ==== */
+
 function initMap() {
   var mapOptions = {
     center: {lat: 51.4275898, lng: -0.308368},
@@ -21,11 +23,16 @@ function initMap() {
 }
 
 
-/*
- *
- * ViewModel
- *
- */
+/* ==== Model Forces ==== */
+
+var Forces = function(data) {
+  this.id = ko.observable(data.id);
+  this.name = ko.observable(data.name);
+}
+
+
+/* ==== ViewModel ==== */
+
  var ViewModel = function() {
   var self = this;
 
@@ -35,46 +42,12 @@ function initMap() {
 
  }
 
-/* Google maps callback sets off the execution of this app */
+
+/* ==== Initialize Google Maps ==== */
+
 window.initApp = function() {
   initMap();
+
   // Initialize the ModelView and bind it to the View.
   ko.applyBindings(new ViewModel());
 }
-
-
-
-/*
-
-async function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-
-  return element;
-}
-
-component().then(component => {
-  document.body.appendChild(component);
-});
-
-let element = component(); // store the element to re-render on print.js change
-document.body.appendChild(element);
-
-if (module.hot) {
-	module.hot.accept('./print.js', function() {
-		console.log('Accepting the updated printMe module!');
-		document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click
-    // handler
-    document.body.appendChild(element);
-	});
-}
-*/
