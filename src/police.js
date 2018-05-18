334ca3b89733989a65fa09164a259d78a86d7a5e
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-05-18 14:26:02 +0100

    Feat: Add error logging

--- src/police.js
+++ src/police.js
@@ -15,7 +15,7 @@ export async function locNeighborhood(lat, lng) {
       `https://data.police.uk/api/locate-neighbourhood?q=` + lat + `,` +
       lng);
   } catch (e) {
-    console.log("Unable to access data.police.uk API");
+    console.log("Unable to access data.police.uk API: " + e.message);
   }
 
   if (response.status !== 200) {
@@ -43,7 +43,11 @@ export async function getBoundsNeigh(neighborhood, force) {
     response = await fetch(`https://data.police.uk/api/` + force + `/` +
       neighborhood + `/boundary`);
   } catch (e) {
-    console.log("Problem loading data. Status code: " + response.status);
+    throw console.log("Problem loading Neighbourhood boundary: " + e.message);
+  }
+
+  if (response.status !== 200) {
+    throw console.log("problem loading data. Status code:" + response.status);
   }
 
   const data = await response.json();
@@ -70,7 +74,11 @@ export async function getStreetLevelCrime(lat, lng, year, month) {
       `https://data.police.uk/api/crimes-street/all-crime?lat=` + lat +
       `&lng=` + lng + `&date=` + year + `-` + month);
   } catch (e) {
-    console.log("Problem loading data. Status code: " + response.status);
+    throw console.log("Problem retrieving Street Level Crime: " + e.message);
+  }
+
+  if (response.status !== 200) {
+    throw console.log("problem loading data. Status code:" + response.status);
   }
 
   const data = await response.json();
@@ -79,28 +87,33 @@ export async function getStreetLevelCrime(lat, lng, year, month) {
   var crimes = [];
 
   // Iterate over the retrieved data and save it in the crimes array
-  data.forEach(function(crime) {
-    // Check if the item.category is in the crimes array
-    var match = ko.utils.arrayFirst(
-      crimes,
-      function(item) {
-        return crime.category === item.category;
-      }
-    );
+  if (data[0] !== undefined && data[0].category !== undefined) {
+    data.forEach(function(crime) {
+      // Check if the item.category is in the crimes array
+      var match = ko.utils.arrayFirst(
+        crimes,
+        function(item) {
+          return crime.category === item.category;
+        }
+      );
 
-    // If there was no match, add the new category with a starting
-    // count of 1
-    // If there was a match increase the count with 1
-    if (!match) {
-      var row = {
-        category: crime.category,
-        count: 1
-      };
-      crimes.push(row);
-    } else {
-      match.count += 1;
-    }
-  });
-  // return the crimes array
-  return crimes;
+      // If there was no match, add the new category with a starting
+      // count of 1
+      // If there was a match increase the count with 1
+      if (!match) {
+        var row = {
+          category: crime.category,
+          count: 1
+        };
+        crimes.push(row);
+      } else {
+        match.count += 1;
+      }
+    });
+    // return the crimes array
+    return crimes;
+  } else {
+    crimes = [{error: "No crimes recorded"}];
+
+  }
 }
